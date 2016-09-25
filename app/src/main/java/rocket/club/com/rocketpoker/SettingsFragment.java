8e099f6ef5ae32ee7565a5a06a67ff5272534e6f
Aryasree <aryasreenr33@gmail.com>
2016-09-25 11:40:03 +0530

    ui changes

--- app/src/main/java/rocket/club/com/rocketpoker/SettingsFragment.java
+++ app/src/main/java/rocket/club/com/rocketpoker/SettingsFragment.java
@@ -2,6 +2,7 @@ package rocket.club.com.rocketpoker;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.android.gcm.GCMRegistrar;
+
 import rocket.club.com.rocketpoker.utils.AppGlobals;
 
 
@@ -22,10 +25,11 @@ public class SettingsFragment extends Fragment {
     Context context = null;
     AppGlobals appGlobals = null;
     TextView dialogMsg;
-    Button okButton;
+    Button okButton,yesbtn,nobtn;
     ToggleButton notifToggle = null, chatToggle = null;
     View.OnClickListener onClickListener = null;
     Dialog dialog = null;
+    LinearLayout signout_layout,profile_layout;
     boolean commonNotif, chatNotif;
 
     @Override
@@ -45,6 +49,10 @@ public class SettingsFragment extends Fragment {
 
         notifToggle = (ToggleButton) view.findViewById(R.id.notifToggle);
         chatToggle = (ToggleButton) view.findViewById(R.id.chatToggle);
+        profile_layout=(LinearLayout) view.findViewById(R.id.profileLayout);
+        signout_layout=(LinearLayout) view.findViewById(R.id.signoutLayout);
+        yesbtn=(Button)view.findViewById(R.id.signoutYes);
+        nobtn=(Button)view.findViewById(R.id.signoutNo);
 
         setNotif(1);
         setNotif(2);
@@ -65,15 +73,46 @@ public class SettingsFragment extends Fragment {
                         setNotif(2);
                         createDialog(getString(R.string.chat_notif_msg));
                         break;
+                    case R.id.signoutLayout:
+                        createSignOutDialog();
+                        break;
                     case R.id.okDialog:
                         dialog.cancel();
                         break;
+                    case R.id.signoutYes:
+                        dialog.dismiss();
+                        redirectToLogin();
+                        break;
+                    case R.id.signoutNo:
+                        dialog.cancel();
+                        break;
+                    case R.id.profileLayout:
+                        Intent intent=new Intent(context,ProfileActivity.class);
+                        startActivity(intent);
+                        break;
+
                 }
             }
         };
 
         notifToggle.setOnClickListener(onClickListener);
         chatToggle.setOnClickListener(onClickListener);
+        profile_layout.setOnClickListener(onClickListener);
+        signout_layout.setOnClickListener(onClickListener);
+    }
+
+    private void redirectToLogin() {
+        appGlobals.sharedPref.setLogInStatus(false);
+        appGlobals.sharedPref.setLoginMobile("");
+        appGlobals.sharedPref.setRegId("");
+        appGlobals.currentFragmentClass = null;
+        GCMRegistrar.setRegisteredOnServer(context, false);
+
+        Intent loginActivity = new Intent(context, LoginActivity.class);
+        loginActivity.putExtra(LoginActivity.pageType, LoginActivity.loginPage);
+        startActivity(loginActivity);
+//        getActivity().getFragmentManager().beginTransaction().remove(SettingsFragment.class).commit();
+        getActivity().finish();
     }
 
     private void setNotif(int setNotif) {
@@ -97,6 +136,21 @@ public class SettingsFragment extends Fragment {
         dialogMsg.setText(msg);
         okButton = (Button) dialog.findViewById(R.id.okDialog);
         okButton.setOnClickListener(onClickListener);
+
+        dialog.show();
+    }
+
+    private void createSignOutDialog() {
+        dialog=new Dialog(context);
+        dialog.setContentView(R.layout.signout_dialogue);
+
+        dialog.setTitle(getString(R.string.signout_heading));
+
+        yesbtn = (Button) dialog.findViewById(R.id.signoutYes);
+        yesbtn.setOnClickListener(onClickListener);
+
+        nobtn = (Button) dialog.findViewById(R.id.signoutNo);
+        nobtn.setOnClickListener(onClickListener);
 
         dialog.show();
     }
