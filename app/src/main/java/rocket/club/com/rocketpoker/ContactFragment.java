d25cca53feaee1e07e9805871f58f50a910f8768
Ravi <aryasreenr33.gmail.com>
2016-09-25 13:25:14 +0530

    added latlng for contact us

--- app/src/main/java/rocket/club/com/rocketpoker/ContactFragment.java
+++ app/src/main/java/rocket/club/com/rocketpoker/ContactFragment.java
@@ -10,13 +10,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ContactFragment extends Fragment {
 
+public class ContactFragment extends Fragment implements OnMapReadyCallback {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        SupportMapFragment fm = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map_locate_us);
+        fm.getMapAsync(this);
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+
+        LatLng latLng = new LatLng(12.965276, 77.641724);
+
+        gMap.moveCamera( CameraUpdateFactory.newLatLngZoom(latLng, 14.0f) );
+        gMap.addMarker(new MarkerOptions().position(latLng));
     }
 }
